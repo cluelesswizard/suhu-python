1c14eb2e443df40d21a00efc7bb98a5566afa5d6
Ilham Taufiq <65374686+iamilham@users.noreply.github.com>
2022-05-16 16:57:26 +0700

    Add files via upload

---
+++ suhu.py
@@ -0,0 +1,18 @@
+print("\nKonverensi Temperatur\n")
+
+# Celcius
+celcius = float(input("masukkan suhu celsius : "))
+print("Suhu adalah : ",celcius," Celcius")
+
+# Reamur
+reamur = (4/5) * celcius
+print("Suhu adalah : ",reamur," Reamur")
+
+# Fahrenheit
+fahrenheit = ((9/5) * celcius)+32
+print("Suhu adalah : ",fahrenheit," Fahrenheit")
+
+# Kelvin
+kelvin = celcius + 273
+print("Suhu adalah : ",kelvin," Kelvin")
+
